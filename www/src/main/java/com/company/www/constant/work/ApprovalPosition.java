10b48a5a7857848cd902d8fd76c31243d3d3abec
Jang seong hwan <jsksh9865@naver.com>
2025-05-09 18:55:54 +0900

    코드 변경
    1. ApprovalPosition
    1-1. 직급 이름에 대한 변수와 그룹에 대한 변수 추가
    2. Converter
    2-1. 확장성 있는 코드로 변경
    2-1-1. switch문에서 Enum 타입.values()로 모든 Enum 타입의 값에 대한 비교로 변경

--- www/src/main/java/com/company/www/constant/work/ApprovalPosition.java
+++ www/src/main/java/com/company/www/constant/work/ApprovalPosition.java
@@ -6,21 +6,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ApprovalPosition {
-    CHAIRMAN(140), // 회장
-    VICE_CHAIRMAN(130), // 부회장
-    PRESIDENT(120), // 사장
-    VICE_PRESIDENT(110), // 부사장
-    SENIOR_MANAGING_DIRECTOR(100), // 전무이사
-    MANAGING_DIRECTOR(90), // 상무이사
-    DIRECTOR(80), // 이사
-    GENERAL_MANAGER(70), // 부장
-    DEPUTY_GENERAL_MANAGER(60), // 차장
-    MANAGER(50), // 과장
-    ASSISTANT_MANAGER(40), // 대리
-    ASSOCIATE(30), // 주임
-    STAFF(20), // 사원
-    INTERN(10), // 인턴
-    NOT_POSITION(0);
+    CHAIRMAN(140, "회장", "대표"), // 회장
+    VICE_CHAIRMAN(130, "부회장", "대표"), // 부회장
+    PRESIDENT(120, "사장", "대표"), // 사장
+    VICE_PRESIDENT(110, "부사장", "대표"), // 부사장
+    SENIOR_MANAGING_DIRECTOR(100, "전무이사", "임원"), // 전무이사
+    MANAGING_DIRECTOR(90, "상무이사", "임원"), // 상무이사
+    DIRECTOR(80, "이사", "임원"), // 이사
+    GENERAL_MANAGER(70, "부장", "중간관리자"), // 부장
+    DEPUTY_GENERAL_MANAGER(60, "차장", "중간관리자"), // 차장
+    MANAGER(50, "과장", "중간관리자"), // 과장
+    ASSISTANT_MANAGER(40, "대리", "실무진"), // 대리
+    ASSOCIATE(30, "주임", "실무진"), // 주임
+    STAFF(20, "사원", "실무진"), // 사원
+    INTERN(10, "인턴", "실무진"), // 인턴
+    NOT_POSITION(0, "미정", "미정");
 
     private final int code;
+    private final String position;
+    private final String group;
 }
--- www/src/main/java/com/company/www/converter/Converter.java
+++ www/src/main/java/com/company/www/converter/Converter.java
@@ -11,11 +11,23 @@ import java.util.List;
 public class Converter {
     // 성별
     public static Gender convetToGender(String gender){
-        switch (gender){
-            case "남자" -> {return Gender.MALE;}
-            case "여자" -> {return Gender.FEMALE;}
-            default -> throw new IllegalStateException("잘못된 데이터입니다.");
+        if (gender == null) {
+            return null;
         }
+
+        String trimmed = gender.trim();
+
+        if (trimmed.isEmpty()) {
+            return null;
+        }
+
+        for (Gender gd : Gender.values()) {
+            if (gd.getCode().equals(trimmed)) {
+                return gd;
+            }
+        }
+        return null;
+
     }
 
     public static String convertToGenderString(Gender gender){
@@ -27,26 +39,18 @@ public class Converter {
         if (approvalState == null) {
             return null;
         }
-
         String trimmed = approvalState.trim();
+
+        if(trimmed.isEmpty()){
+            return null;
+        }
+
         for (ApprovalState state : ApprovalState.values()) {
             if (state.getCode().equals(trimmed)) {
                 return state;
             }
         }
         return null;
-        /*
-        return switch(approvalState){
-            case "최종결재" -> ApprovalState.FINALIZATION;
-            case "결재" -> ApprovalState.APPROVE;
-            case "보류" -> ApprovalState.DEFER;
-            case "반려" -> ApprovalState.REJECT;
-            case "전결" -> ApprovalState.ARBITRARY;
-            case "대결" -> ApprovalState.PROXY;
-            case "전대결" -> ApprovalState.ARBITRARY_PROXY;
-            default -> null;
-        };
-        */
     }
 
     public static String convertToApprovalStateString(ApprovalState approvalState){
@@ -55,16 +59,22 @@ public class Converter {
 
     // 결재 상태
     public static WorkState convertToWorkState(String workState){
-        String trimmed = workState.trim();
-        switch (trimmed){
-            case "결재대기" -> { return WorkState.PENDING; }
-            case "결재중" -> { return  WorkState.HANDLING; }
-            case "결재완료" -> { return WorkState.COMPLETED; }
-            case "공람" -> { return WorkState.DISPLAY; }
-            case "보존년한 만기" -> { return WorkState.OVERDUE; }
-            case "삭제" -> { return WorkState.DELETE; }
-            default -> { return null; }
+        if(workState == null){
+            return null;
         }
+
+        String trimmed = workState.trim();
+
+        if(trimmed.isEmpty()){
+            return null;
+        }
+
+        for(WorkState state : WorkState.values()){
+            if(state.getCode().equals(trimmed)){
+                return state;
+            }
+        }
+        return null;
     }
 
     public static String convertToWorkStateString(WorkState workState){
@@ -73,42 +83,27 @@ public class Converter {
 
     // 결재 직급
     public static String convertToApprovalPositionString(ApprovalPosition approvalPosition){
-        switch (approvalPosition){
-            case NOT_POSITION -> { return "미정"; }
-            case CHAIRMAN -> { return "회장"; }
-            case VICE_CHAIRMAN -> { return "부회장"; }
-            case PRESIDENT -> { return "사장"; }
-            case VICE_PRESIDENT -> { return "부사장"; }
-            case SENIOR_MANAGING_DIRECTOR -> { return "전무이사"; }
-            case MANAGING_DIRECTOR -> { return "상무이사"; }
-            case DIRECTOR -> { return "이사"; }
-            case GENERAL_MANAGER -> { return "부장"; }
-            case DEPUTY_GENERAL_MANAGER -> { return "차장"; }
-            case MANAGER -> { return "과장"; }
-            case ASSISTANT_MANAGER -> { return "대리"; }
-            case ASSOCIATE -> { return "주임"; }
-            case STAFF -> { return "사원"; }
-            case INTERN -> { return "인턴";}
-            default -> { return null; }
-        }
+        return approvalPosition.getPosition();
     }
 
     // 휴가
     public static HolidayType convertToHolidayType(String holidayType){
-        switch (holidayType){
-            case "연차" -> { return HolidayType.ANNUAL; }
-            case "보상" -> { return HolidayType.REWORD; }
-            case "가족돌봄" -> { return HolidayType.FAMILY_CARE; }
-            case "유산&사산" -> { return HolidayType.MISCARRIAGE_STILLBIRTH; }
-            case "출산" -> { return HolidayType.CHILDBIRTH; }
-            case "배우자 출산" -> { return HolidayType.SPOUSE_CHILDBIRTH; }
-            case "난임치료" -> { return HolidayType.FERTILITY_TREATMENT; }
-            case "공가" -> { return HolidayType.OFFICIAL; }
-            case "병가" -> { return HolidayType.SICK; }
-            case "경조사" -> { return HolidayType.FAMILY_EVENT; }
-            case "여름휴가" -> { return HolidayType.SUMMER; }
-            default -> { return HolidayType.OTHER; }
+        if(holidayType == null){
+            return null;
         }
+
+        String trimmed = holidayType.trim();
+
+        if(trimmed.isEmpty()){
+            return null;
+        }
+
+        for(HolidayType type : HolidayType.values()){
+            if(type.getCode().equals(trimmed)){
+                return type;
+            }
+        }
+        return HolidayType.OTHER;
     }
 
     public static String convertToHolidayTypeString(HolidayType holidayType){
@@ -117,12 +112,22 @@ public class Converter {
     
     // 출근 상태
     public static CommuteType convertToCommuteType(String commuteType){
-        switch (commuteType){
-            case "출근" -> { return CommuteType.COMMUTE; }
-            case "지각" -> { return CommuteType.LATENESS; }
-            case "결근" -> { return CommuteType.ABSENCE; }
-            default ->  { return null; }
+        if(commuteType == null){
+            return null;
         }
+
+        String trimmed = commuteType.trim();
+
+        if(trimmed.isEmpty()){
+            return null;
+        }
+
+        for(CommuteType type : CommuteType.values()){
+            if(type.getCode().equals(trimmed)){
+                return type;
+            }
+        }
+        return null;
     }
     public static String convertToCommuteTypeString(CommuteType commuteType){
         return commuteType.getCode();
